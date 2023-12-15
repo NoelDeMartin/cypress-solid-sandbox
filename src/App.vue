f373195f155e58be96521bf1f1097bf86a7cd7d6
Noel De Martin <noel@noeldemartin.com>
2023-12-15 10:56:27 +0100

    Implement Solid login

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
     <AGAppLayout class="bg-blue-50">
         <main class="flex flex-grow flex-col items-center justify-center">
-            <h1 class="text-4xl font-semibold">
-                {{ $t('home.title') }}
-            </h1>
-            <a href="https://aerogel.js.org" target="_blank" class="mt-2 underline opacity-75 hover:opacity-100">
-                {{ $t('home.getStarted') }}
-            </a>
+            <div v-if="$solid.isLoggedIn()" class="flex flex-col items-center gap-3">
+                <h1 class="fontsmi prose text-xl">
+                    <AGMarkdown
+                        inline
+                        :text="`Hello, [${$solid.user.name ?? $solid.user.webId}](${$solid.user.webId})`"
+                    />
+                </h1>
+                <AGButton @click="$solid.logout(true)">
+                    Log out
+                </AGButton>
+            </div>
+            <AGForm
+                v-else
+                :form="form"
+                class="flex flex-col items-center gap-3"
+                @submit="$ui.loading($solid.login(form.url))"
+            >
+                <h1 class="fontsmi text-xl">
+                    Log in with Solid
+                </h1>
+                <div class="flex">
+                    <AGInput name="url" placeholder="Your WebId" aria-label="Login url" />
+                    <AGButton submit>
+                        Log In
+                    </AGButton>
+                </div>
+                <p v-if="$solid.error" class="text-red">
+                    {{ $errors.getErrorMessage($solid.error) }}
+                </p>
+            </AGForm>
         </main>
     </AGAppLayout>
 </template>
+
+<script setup lang="ts">
+import { requiredStringInput, useForm } from '@aerogel/core';
+
+const form = useForm({ url: requiredStringInput() });
+</script>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { bootstrap } from '@aerogel/core';
 
 import './assets/css/styles.css';
 import App from './App.vue';
+import solid from '@aerogel/plugin-solid';
 
 bootstrap(App, {
     plugins: [
         i18n({ messages: import.meta.glob('@/lang/*.yaml') }),
         soukai({ models: import.meta.glob(['@/models/*', '!**/*.test.ts'], { eager: true }) }),
+        solid({ autoReconnect: true }),
     ],
 });
